35fbca077dd5d7ea3d5ada6bd182e0fd75c5ad6c
Vadim Goryainov <vg.uni@t-online.de>
2018-04-17 16:14:06 +0200

    image requests

--- src/Server.java
+++ src/Server.java
@@ -1,8 +1,16 @@
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.util.Map;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
@@ -10,10 +18,27 @@ import com.sun.net.httpserver.HttpServer;
 
 
 public class Server {
+	
+	static Document baseDoc;
 
+	
+	// localhost:8000/sendPoints?points=p
 	public static void main(String[] args) throws Exception {
+		baseDoc = Jsoup.parse(new File("base.html"),"UTF-8");
+		
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         HttpContext context = server.createContext("/sendPoints", new MyHandler());
+
+        File imgDir = new File("./images");
+        for (String filename : imgDir.list())
+        {
+        	File f = new File("./images/"+filename);
+        	if (f.isFile())
+        	{
+        		System.out.println("Found file: "+filename);
+        		server.createContext("/"+filename, new ImageHandler(filename));
+        	}
+        }
         server.setExecutor(null);
         context.getFilters().add(new ParameterFilter());
         System.out.println("Server Running.");
@@ -26,21 +51,54 @@ public class Server {
         	@SuppressWarnings("unchecked")
 			Map<String, Object> params = (Map<String, Object>)t.getAttribute("parameters");
         	
-        	String response = "";
+        	Element respDiv = new Element("div");
         	if (params.get("points") == null)
         	{
-        		response = "Points not found.";
+        		respDiv.text("Points not found.");
         	}
         	else
         	{
-        		System.out.println(params.get("points").toString());
-        		response = "Points received.";
+        		respDiv.text(params.get("points").toString());
         	}
+        	Document newDoc = baseDoc.clone();
+        	respDiv.appendTo(newDoc.body());
+        	String response = newDoc.toString();
+        	
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
             t.close();
+        }
+    }
+    
+    static class ImageHandler implements HttpHandler{
+    	
+    	String filename;
+    	
+    	public ImageHandler(String filename) {
+    		this.filename = filename;
+    	}
+    	 
+        @Override
+        public void handle(HttpExchange he) throws IOException {
+        	
+            Headers headers = he.getResponseHeaders();
+            headers.add("Content-Type", "image/png");
+             
+            File file = new File ("./images/"+filename);
+            byte[] bytes  = new byte [(int)file.length()];
+            System.out.println(file.getAbsolutePath());
+            System.out.println("length:" + file.length());
+             
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+            bufferedInputStream.read(bytes, 0, bytes.length);
+            bufferedInputStream.close();
+ 
+            he.sendResponseHeaders(200, file.length());
+            OutputStream outputStream = he.getResponseBody();
+            outputStream.write(bytes, 0, bytes.length);
+            outputStream.close();
         }
     }
 
