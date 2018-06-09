cb1d26dc077bb5a13d6a00126b5aac9e8e672e96
Vadim Goryainov <vg.uni@t-online.de>
2018-06-09 19:05:59 +0200

    pia server v 0.1

---
+++ Procfile
@@ -0,0 +1,1 @@
+java -jar pia-server-1.0-shaded.jar
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -14,50 +14,49 @@ import java.util.Random;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
+import com.google.gson.Gson;
+
 public class Server {
 	
-	static Document baseDoc;
 	public static HttpServer server;
+	
+	public static final String pia_orange = "rgb(242, 132, 55)";
+	
+	public static final int errorCode = 210;
 	
 	// localhost:8000/sendPoints?points=1,2,3,4&nick=maxmuster&gameId=jlrnv
 	// localhost:8000/createGame
 	// localhost:8000/gameStats?gameId=jlrnv
-	public static void main() throws Exception {
+	public static void main(String[] args) throws Exception {
 		//Server.createImages("jlrnv");
-		baseDoc = Jsoup.parse(new File("base.html"),"UTF-8");
+		
 		//JavaFXApplication.launch(args);
 		
         server = HttpServer.create(new InetSocketAddress(8000), 0);
         server.createContext("/sendPoints", new PointHandler()).getFilters().add(new ParameterFilter());
-        server.createContext("/images/", new ImageHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/gameInfo", new GameInfoHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/images/", new ImageHandler());
         server.createContext("/createGame", new GameCreateHandler()).getFilters().add(new ParameterFilter());
         server.createContext("/gameStats", new ResultsHandler()).getFilters().add(new ParameterFilter());
-        server.createContext("/test", new TestHandler()).getFilters().add(new ParameterFilter());
+        server.createContext("/scripts/", new ScriptHandler());
+        server.createContext("/", new IndexHandler());
+        server.createContext("/styles/", new StyleHandler());
         
         server.setExecutor(null);
         System.out.println("Server Running.");
         server.start();
     }
 	
-	/*
-	static void createImages(String gameId)
-	{
-		GameStats stats = Server.ResultsHandler.loadGame("jlrnv");
-    	JavaFXApplication.stage.fireEvent(new ImageCreationEvent("jlrnv", stats));
-	}
-	*/
-	
-	static class TestHandler implements HttpHandler {
+	static class IndexHandler implements HttpHandler {
 		@Override
 		public void handle(HttpExchange t) throws IOException {
-			Document doc = Jsoup.parse(new File("imgtest.html"), "UTF-8");
+			Document doc = Jsoup.parse(new File("html/index.html"), "UTF-8");
 			String response = doc.toString();
 			t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
@@ -67,12 +66,10 @@ public class Server {
 		}
 	}
 	
-	
-	public static class ResultsHandler implements HttpHandler {
+	public static class GameInfoHandler implements HttpHandler {
+		
 		@Override
 		public void handle(HttpExchange t) throws IOException {
-			Document doc = Jsoup.parse(new File("results_base.html"), "UTF-8");
-			
 			@SuppressWarnings("unchecked")
 			Map<String, Object> params = (Map<String, Object>)t.getAttribute("parameters");
 			if (!params.containsKey("gameId"))
@@ -84,38 +81,36 @@ public class Server {
 			String gameId = (String)params.get("gameId");
 			
 			GameStats stats = loadGame(gameId);
-			System.out.println("Stats: "+stats.toString());
 			
+			Gson gson = new Gson();
 			
-			String response = doc.toString();
+			LinkedList<String> labels = new LinkedList<>();
+			LinkedList<Integer> data = new LinkedList<>();
+			for (int i = 0; i < stats.namePointPairs.length; i++)
+			{
+				labels.add(stats.namePointPairs[i].name);
+				data.add(stats.namePointPairs[i].points);
+			}
+			ChartsDataset cd = new ChartsDataset(labels.toArray(new String[0]), data.toArray(new Integer[0]), "Punkte", Server.pia_orange, Server.pia_orange);
+			
+			String response = gson.toJson(cd);
 			t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
             t.close();
-			
 		}
 		
 		static void sendFail(String msg, HttpExchange t) throws IOException
         {
         	String response = "Failed requesting game stats: "+msg;
-            t.sendResponseHeaders(200, response.length());
+            t.sendResponseHeaders(errorCode, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
             t.close();
         }
-		
-		
-		static void addImagesToDoc(String gameId, Document doc)
-		{
-			Element imageElement = doc.body().getElementById("platzierung");
-	        imageElement.attr("src", "/images/"+gameId+"/placement.png");
-	        imageElement.appendTo(doc.body());
 
-			doc.title("Spielstatistiken von "+gameId);
-		}
-		
 		public static GameStats loadGame(String gameId)
 		{
 			File gameDir = new File("./games/"+gameId);
@@ -158,6 +153,57 @@ public class Server {
 	    	GameStats stats = new GameStats(allPoints.toArray(new NamePointPair[0]));
 			return stats;
 		}
+	}
+	
+	
+	public static class ResultsHandler implements HttpHandler {
+		@Override
+		public void handle(HttpExchange t) throws IOException {
+			Document doc = Jsoup.parse(new File("html/results_base.html"), "UTF-8");
+			
+			@SuppressWarnings("unchecked")
+			Map<String, Object> params = (Map<String, Object>)t.getAttribute("parameters");
+			if (!params.containsKey("gameId"))
+			{
+				System.out.println("gameId not found");
+				sendFail("gameId not found", t);
+				return;
+			}
+			String gameId = (String)params.get("gameId");
+			
+			File gameDir = new File("./games/"+gameId);
+        	if (!gameDir.isDirectory())
+        	{
+        		System.out.println("Couldn't find game "+gameId);
+        		sendFail("Couldn't find game "+gameId, t);
+        	}
+			
+        	else
+        	{
+        		String response = doc.toString();
+    			t.sendResponseHeaders(200, response.length());
+                OutputStream os = t.getResponseBody();
+                os.write(response.getBytes());
+                os.close();
+                t.close();
+        	}
+
+		}
+		
+		
+		
+		static void sendFail(String msg, HttpExchange t) throws IOException
+        {
+        	String response = "Failed requesting game stats: "+msg;
+            t.sendResponseHeaders(errorCode, response.length());
+            OutputStream os = t.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+            t.close();
+        }
+		
+		
+		
 	}
 	
     static class PointHandler implements HttpHandler {
@@ -215,7 +261,7 @@ public class Server {
         static void sendFail(String msg, HttpExchange t) throws IOException
         {
         	String response = "Failed adding points: "+msg;
-            t.sendResponseHeaders(200, response.length());
+            t.sendResponseHeaders(errorCode, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
             os.close();
@@ -283,6 +329,60 @@ public class Server {
         }
     }
     
+    static class ScriptHandler implements HttpHandler{
+    	
+    	@Override
+        public void handle(HttpExchange he) throws IOException {
+        	
+            Headers headers = he.getResponseHeaders();
+            headers.add("Content-Type", "text/javascript");
+            
+            String uriPath = he.getRequestURI().getPath();
+            String filename = "."+uriPath;
+            
+            File file = new File (filename);
+            byte[] bytes  = new byte [(int)file.length()];
+             
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+            bufferedInputStream.read(bytes, 0, bytes.length);
+            bufferedInputStream.close();
+ 
+            he.sendResponseHeaders(200, file.length());
+            OutputStream outputStream = he.getResponseBody();
+            outputStream.write(bytes, 0, bytes.length);
+            outputStream.close();
+        }
+    	
+    	
+    }
+    
+    static class StyleHandler implements HttpHandler{
+    	
+    	@Override
+        public void handle(HttpExchange he) throws IOException {
+        	
+            Headers headers = he.getResponseHeaders();
+            headers.add("Content-Type", "text/css");
+            
+            String uriPath = he.getRequestURI().getPath();
+            String filename = "."+uriPath;
+            
+            File file = new File (filename);
+            byte[] bytes  = new byte [(int)file.length()];
+             
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(file));
+            bufferedInputStream.read(bytes, 0, bytes.length);
+            bufferedInputStream.close();
+ 
+            he.sendResponseHeaders(200, file.length());
+            OutputStream outputStream = he.getResponseBody();
+            outputStream.write(bytes, 0, bytes.length);
+            outputStream.close();
+        }
+    	
+    	
+    }
+    
     static class GameCreateHandler implements HttpHandler
     {
     	
@@ -319,6 +419,7 @@ public class Server {
         
         static void createGame(String id, HttpExchange t) throws IOException
     	{
+        	Document doc = Jsoup.parse(new File("html/gameCreated.html"), "UTF-8");
     		File gameDir = new File("./games/"+id);
     		if (gameDir.isDirectory())
     		{
@@ -330,7 +431,14 @@ public class Server {
     			System.out.println("Couldn't create game "+id);
     			return;
     		}
-    		String response = "Game created: "+id;
+    		
+    		doc.getElementById("gameIDText").text(id);
+    		String link = server.getAddress().getHostString()+ "/gameStats?gameId=" + id;
+    		doc.getElementById("gameIDLink").text(link);
+    		doc.getElementById("gameIDLink").attr("href", "/gameStats?gameId=" + id);
+    		
+    		String response = doc.toString();
+    		
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
             os.write(response.getBytes());
