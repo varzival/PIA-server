84688abee8957481e4d21d71e37f398a38c6f4e4
Vadim Goryainov <vg.uni@t-online.de>
2018-06-13 01:29:53 +0200

    server frontend

--- src/main/java/ChartsDataset.java
+++ src/main/java/ChartsDataset.java
@@ -6,23 +6,23 @@ public class ChartsDataset {
 	class Dataset
 	{
 		String label;
-		String backgroundColor;
-		String borderColor;
-		int borderWidth;
+		String[] backgroundColor;
+		//String borderColor;
+		//int borderWidth;
 		Integer[] data;
 	}
 	
 	Dataset[] datasets;
 
-	public ChartsDataset(String[] labels, Integer[] data, String datasetLabel, String backgroundColor, String borderColor) {
+	public ChartsDataset(String[] labels, Integer[] data, String datasetLabel, String[] backgroundColor) {
 		this.labels = labels;
 
 		Dataset set = new Dataset();
 		set.label = datasetLabel;
 		set.data = data;
 		set.backgroundColor = backgroundColor;
-		set.borderColor = borderColor;
-		set.borderWidth = 1;
+		//set.borderColor = borderColor;
+		//set.borderWidth = 1;
 		
 		datasets = new Dataset[1];
 		datasets[0] = set;
